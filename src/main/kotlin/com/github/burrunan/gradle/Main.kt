8697f71e9edaf9e7e758c03e46f071a65ae7fc68
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-07-30 09:59:46 +0300

    wip

--- src/main/kotlin/com/github/burrunan/gradle/Main.kt
+++ src/main/kotlin/com/github/burrunan/gradle/Main.kt
@@ -1,5 +1,6 @@
 package com.github.burrunan.gradle
 
+import github.actions.core.info
 import github.actions.core.warning
 import process
 
@@ -12,6 +13,6 @@ suspend fun main() {
         warning("$RefKey is needed for the action to work. GitHub event was ${process.env["GITHUB_EVENT_NAME"]}")
         return
     }
-
+    info(process.env["GITHUB_EVENT_PATH"] ?: "no event")
     GradleCacheAction(process.env[RefKey] ?: "").run()
 }
