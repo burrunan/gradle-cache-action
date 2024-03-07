86e56cce2ea632e8f4e1c12717429a586cb8dcb8
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2024-03-07 14:22:25 +0300

    chore: bump kotlin to 1.9.23

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,8 @@
 
 pluginManagement {
     plugins {
-        kotlin("multiplatform") version "1.9.22"
-        kotlin("plugin.serialization") version "1.9.22"
+        kotlin("multiplatform") version "1.9.23"
+        kotlin("plugin.serialization") version "1.9.23"
     }
 }
 
