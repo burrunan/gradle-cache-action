abb09c94a7a122407f3f05389c9372fce9cb7aee
Mend Renovate <renovate@whitesourcesoftware.com>
2024-03-09 13:56:52 +0100

    fix(deps): update dependency org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom to v1.0.0-pre.710 (#76)

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ allprojects {
         dependencies {
             "commonMainApi"(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.8.0"))
             "commonMainApi"(platform("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.6.3"))
-            "jsMainImplementation"(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.709"))
+            "jsMainImplementation"(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.710"))
             if (project.path != ":test-library") {
                 "jsTestImplementation"(rootProject.projects.testLibrary)
             }
